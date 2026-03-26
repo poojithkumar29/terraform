4f6734d9777162382cb6537e506e6ec4ae58b569
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-03-26 13:26:42 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "terraform" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
+resource "aws_security_group" "allow-all" {
+  name   = "allow-all"
+
+
+  egress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  ingress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  tags = {
+    Name = "allow-all"
+  }
+}
